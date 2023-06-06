005e85cb9190c936e257cae3d5891e55f36e331f
daria_borodina <dasha.borodina2003@mail.ru>
2023-06-06 12:04:29 +0300

    Revert "feat: add i18next"
    This reverts commit 8b125065c1f952a6c09ada4f73190f7ba6d4031d.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
-const { i18n } = require('./next-i18next.config')
 
 const nextConfig = {
-  i18n,
-  sassOptions: {
-    includePaths: [path.join(__dirname,'src', 'styles')],
-    prependData: `@import "variables.scss";`
-  },
+    sassOptions: {
+          includePaths: [path.join(__dirname,'src', 'styles')],
+          prependData: `@import "variables.scss";`
+        }
 }
 
 module.exports = nextConfig
--- src/app/i18n/settings.ts
+++
@@ -1,3 +0,0 @@
-export const fallbackLng = 'en';
-export const languages = [fallbackLng, 'ru'];
-export const defaultNS = 'translation';
